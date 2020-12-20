a236a082f837bf347bed30db8233c7bd5185e6a7
Pablo Antonini <pabloantoninivazquez@gmail.com>
2020-12-20 19:20:20 +0100

    Created Navbar skeleton

--- src/Components/Header/Navbar/Navbar.jsx
+++ src/Components/Header/Navbar/Navbar.jsx
@@ -1,11 +1,31 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 
 function Navbar() {
     return (
-        <div>
-        <Navbar />
-        </div>
+        <>
+        <Nav>
+            <NavLink to="/">
+                <h1>Logo</h1>
+            </NavLink>
+            <Bars />
+            <NavMenu>
+                <NavLink to="/about" activeStyle>
+                    About
+                </NavLink>
+                <NavLink to="/services" activeStyle>
+                    Services
+                </NavLink>
+                <NavLink to="/people" activeStyle>
+                    People
+                </NavLink>
+            </NavMenu>
+            <NavBtn>
+                <NabBtnLink to="/login">Logout</NabBtnLink>
+            </NavBtn>
+        </Nav>
+        </>
     )
 }
 
